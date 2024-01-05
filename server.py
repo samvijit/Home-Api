e77711bb1ed1da519c450f8761a8f5c04dd003d4
sonaltoppo <88210372+samvijit@users.noreply.github.com>
2024-01-05 11:23:44 +0530

    Update server.py

--- server.py
+++ server.py
@@ -6,4 +6,10 @@ def home():
     return jsonify(msg="Hello World")
 
 if __name__=="__main__":
-    app.run()
+    from os import getenv
+    from dotenv import load_dotenv
+    load_env()
+    h = getenv("HOST")
+    p = getnev("PORT")
+    d = getenv("DEBUG")
+    app.run(host=h,port=p,debug=d)
